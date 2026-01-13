2bf1b23a465d9948582c9fe927ddecc6251239a7
Saurav Mehrotra <smehrotra3@atlassian.com>
2026-01-13 15:29:34 +0530

    Add flaky test

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+  @Test
+  void test() {
+    int i = new Random().nextInt(10);
+    assertTrue(i >= 5);
+  }
+}
